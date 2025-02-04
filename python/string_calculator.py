b1bd01875cdba09abb09bc26a2f62c926f554d39
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-04 20:58:04 +0530

    Comma seperated string input is passing

--- python/string_calculator.py
+++ python/string_calculator.py
@@ -6,13 +6,18 @@ class StringCalculator:
 
     def add(self, inp_str: str) -> int:
         acc: int = 0
+        numbers: List[int] = []
         if inp_str == "":
             return 0
 
-        try:
-            cur_num: int = int(inp_str)
-            acc += cur_num
-        except ValueError:
-            raise ValueError("Invalid number")
+        for num_str in inp_str.split(","):
+            try:
+                cur_num: int = int(num_str)
+                numbers.append(cur_num)
+            except ValueError:
+                raise ValueError("Invalid number: {num_str}")
+
+        for num in numbers:
+            acc += num
 
         return acc
