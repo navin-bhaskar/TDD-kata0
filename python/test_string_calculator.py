6bd124acfe9a318c01469f1017dead3c7cd70eb4
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-04 22:34:04 +0530

    Minor cleanup

--- python/test_string_calculator.py
+++ python/test_string_calculator.py
@@ -3,7 +3,7 @@ import unittest
 from string_calculator import StringCalculator
 
 
-class StringCalcTester(unittest.TestCase):
+class TestStringCalc(unittest.TestCase):
 
     def test_pass_empty_string_to_add__get_back_0(self):
         sc = StringCalculator()
