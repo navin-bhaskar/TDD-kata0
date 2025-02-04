3116821ce0acb0dd3789a622fb4efc50d68e8e55
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-04 23:04:23 +0530

    Add UT for checking custom delimeters

--- python/test_string_calculator.py
+++ python/test_string_calculator.py
@@ -7,33 +7,33 @@ class TestStringCalc(unittest.TestCase):
 
     def test_pass_empty_string_to_add__get_back_0(self):
         sc = StringCalculator()
-        assert sc.add("") == 0
+        self.assertEqual(sc.add(""), 0)
 
     def test_input_single_digit_str__get_back_same_digit_as_int(self):
         sc = StringCalculator()
         out = sc.add("1")
-        assert isinstance(out, int)
-        assert out == 1
+        self.assertEqual(isinstance(out, int), True)
+        self.assertEqual(out, 1)
 
     def test_pass_comma_seperated_ints__add_returns_sum_of_ints(self):
         sc = StringCalculator()
-        assert sc.add("1,5") == 6
+        self.assertEqual(sc.add("1,5"), 6)
 
         inp_str: str = ""
         for n in range(1, 11):
             expected_sum = n * (n + 1) // 2
             inp_str = inp_str + str(n)
-            assert sc.add(inp_str) == expected_sum
+            self.assertEqual(sc.add(inp_str), expected_sum)
             inp_str = inp_str + ","
 
     def test_add_input_has_new_line__new_line_also_handled_as_seperator(self):
         sc = StringCalculator()
 
-        assert sc.add("1\n2,3") == 6
+        self.assertEqual(sc.add("1\n2,3"), 6)
 
     def test_add_input_has_custom_delimeter__custom_delimeter_is_used(self):
         sc = StringCalculator()
-        assert sc.add("//;\n1;2") == 3
+        self.assertEqual(sc.add("//;\n1;2"), 3)
 
     def test_input_string_has_negative_numbers__add_throws_excpetion_with_message(self):
         sc = StringCalculator()
@@ -55,4 +55,9 @@ class TestStringCalc(unittest.TestCase):
         self,
     ):
         sc = StringCalculator()
-        assert sc.add("1\n2,1000,1001,3") == 6
+        self.assertEqual(sc.add("1\n2,1000,1001,3"), 6)
+
+    def test_input_has_custom_delimeter__custom_delimeters_ar_used(self):
+        sc = StringCalculator()
+
+        self.assertEqual(sc.add("//[***]\n1***2***3"), 6)
