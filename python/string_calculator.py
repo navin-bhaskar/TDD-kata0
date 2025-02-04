1a668ccf3405516f09ee57964d624c437da69737
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-04 20:16:09 +0530

    Second test case passing

--- python/string_calculator.py
+++ python/string_calculator.py
@@ -1,6 +1,18 @@
+from typing import List
+
+
 class StringCalculator:
     """simple string calculator class"""
 
     def add(self, inp_str: str) -> int:
+        acc: int = 0
+        if inp_str == "":
+            return 0
 
-        return 0
+        try:
+            cur_num: int = int(inp_str)
+            acc += cur_num
+        except ValueError:
+            raise ValueError("Invalid number")
+
+        return acc
