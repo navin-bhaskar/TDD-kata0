5983941a7c1f864d3a089b98333e731419fb95d8
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-21 16:13:05 +0530

    Req4. test case passes

--- python/string_calculator.py
+++ python/string_calculator.py
@@ -5,8 +5,8 @@ from typing import List
 class StringCalculator:
 
     @staticmethod
-    def _get_numbers(inp_str: str) -> List[int]:
-        reg_exp: str = r",|\n"
+    def _get_numbers(inp_str: str, delem: str) -> List[int]:
+        reg_exp: str = delem + r"|\n"
         nums_strs: List[str] = re.split(reg_exp, inp_str)
 
         nums: List[int] = []
@@ -16,12 +16,26 @@ class StringCalculator:
                 nums.append(int(num_str))
             except ValueError:
                 raise ValueError(f"Not a valid number {num_str}")
-            
         return nums
 
+    @staticmethod
+    def _get_delem(inp_str: str):
+        if inp_str.startswith("//"):
+            delem: str = inp_str[2]
+        else:
+            delem = ","
+        return delem
+
+    @staticmethod
+    def _clean_input(inp_str: str):
+        if inp_str.startswith("//"):
+            return inp_str[4:]
+        return inp_str
 
     def add(self, inp_str: str) -> int:
         if inp_str == "":
             return 0
-        nums = StringCalculator._get_numbers(inp_str)
+        delem = StringCalculator._get_delem(inp_str)
+        inp_str = StringCalculator._clean_input(inp_str)
+        nums = StringCalculator._get_numbers(inp_str, delem)
         return sum(nums)
