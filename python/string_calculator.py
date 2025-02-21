7b9eeece426a1c4d2fb579a51edadfbdb4e96914
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-21 15:51:33 +0530

    First tet case

---
+++ python/string_calculator.py
@@ -0,0 +1,5 @@
+
+class StringCalculator:
+    def add(self, inp_str: str) -> int:
+        if inp_str == "":
+            return 0
--- python/test_string_calculator.py
+++ python/test_string_calculator.py
@@ -1,7 +1,17 @@
 import unittest
 
-from string_calculator import StringCalc
+from string_calculator import StringCalculator
 
 
 class TestStringCalc(unittest.TestCase):
-    pass
+    
+    def test__add_given_comma_seperated_num_str__returns_sum(self):
+        """ The method can take up to two numbers, separated by commas, and will return their sum.
+        for example “” or “1” or “1,2” as inputs.
+        (for an empty string it will return 0)"""
+        sc = StringCalculator()
+        res = sc.add("")
+        assert res == 0
+
+        res = sc.add("1")
+        assert res == 1
